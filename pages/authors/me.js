e3c7b9aa61c79399debff3b4ba18f44beac64ccb
estong jamion <estong.jamion@gmail.com>
2023-05-08 20:00:30 +0300

    Initial Commit

---
+++ pages/authors/me.js
@@ -0,0 +1,3 @@
+export default function Me() {
+  return <h1>This is me</h1>;
+}
---
+++ pages/posts/another-page.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/layout";
+
+export default function AnotherPost() {
+  return (
+    <Layout>
+      <h1>AYYEEEEEEE!</h1>;
+    </Layout>
+  );
+}
